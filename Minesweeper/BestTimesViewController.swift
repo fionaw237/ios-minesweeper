264d8ab9709473981a6123f274ba7a3cad56b17d
Fiona Wilson <fiona.wilson@resdiary.com>
2019-08-20 15:26:40 +0100

    Minor adjustments for picker view

--- Minesweeper/BestTimesViewController.swift
+++ Minesweeper/BestTimesViewController.swift
@@ -14,16 +14,13 @@ class BestTimesViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var bestTimesTableView: UITableView!
     @IBOutlet weak var pickerView: UIPickerView!
     
-    var pickerData: [String] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        pickerData = ["Beginner", "Intermediate", "Advanced"]
-    }
+    var pickerData = ["Beginner", "Intermediate", "Advanced"]
     
     @IBAction func menuButtonPressed(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true, completion:nil)
     }
+    
+    // MARK: table view delegate methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
@@ -33,6 +30,8 @@ class BestTimesViewController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: BestTimesTableViewCell.self), for: indexPath) as! BestTimesTableViewCell
         return cell
     }
+    
+    // MARK: picker view delegate methods
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -44,5 +43,9 @@ class BestTimesViewController: UIViewController, UITableViewDelegate, UITableVie
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
     }
 }
