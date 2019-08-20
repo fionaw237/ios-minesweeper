046536180e8bd88e7cc861d92a7e3e4dea06137e
Fiona Wilson <fiona.wilson@resdiary.com>
2019-08-20 13:54:23 +0100

    Set up class for best times table view cell and linked with table view

---
+++ Minesweeper/BestTimesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BestTimesTableViewCell.swift
+//  Minesweeper
+//
+//  Created by Fiona Wilson on 20/08/2019.
+//  Copyright © 2019 Fiona Wilson. All rights reserved.
+//
+
+import UIKit
+
+class BestTimesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+}
--- Minesweeper/BestTimesViewController.swift
+++ Minesweeper/BestTimesViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class BestTimesViewController: UIViewController {
+class BestTimesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var bestTimesTableView: UITableView!
     
     @IBAction func menuButtonPressed(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true, completion:nil)
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: BestTimesTableViewCell.self), for: indexPath) as! BestTimesTableViewCell
+        return cell
+    }
+
 }
